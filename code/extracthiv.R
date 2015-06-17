6de424977a222b58d0bb4d8e4e9cdd2d82427728
Gavin O'Brien <gobrien@servings.org>
2015-06-17 16:54:53 -0400

    further work on pulling prevalence numbers

--- code/extracthiv.R
+++ code/extracthiv.R
@@ -1,6 +1,9 @@
-Lines <- readLines("data/hsicity-townabington.rtf")
+Lines <- readLines("data/hsicity-townboston.rtf")
 # match <- sub("HIV\\/AIDS Prevalence\\\\\\~", Lines, value = TRUE)
 match <- grep(".*HIV\\/AIDS Prevalence\\\\\\~", Lines)
 match.result <- Lines[match + 1]
-# match.rate <- gsub("\\\\\\~..", "\\1", match.result)
-match.rate <- gsub(".*\\\\\\~(...).*", "\\1", match.result)
+# match.rate <- gsub(".*\\\\\\~(.*?\\\\\\cell).*", "\\1", match.result)
+match.rate <- gsub(".*\\\\\\~.(.*?)\\\\cell.*", "\\1", match.result)
+match.rate <- as.integer(gsub(",", "", match.rate))
+
+
