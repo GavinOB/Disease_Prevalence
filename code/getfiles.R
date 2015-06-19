816b9611e6bf43f6cc1a099ebabf689678fb5f25
Gavin O'Brien <geobrien@gmail.com>
2015-06-18 20:16:32 -0400

    added data frame of town names and filenames

--- code/getfiles.R
+++ code/getfiles.R
@@ -1,15 +1,21 @@
 library(XML) # HTML processing
 options(stringsAsFactors = FALSE)
 
-# Base URL
+# Set location variables
 source.url <- "http://www.mass.gov/eohhs/researcher/community-health/masschip/health-status-indicators.html"
 base.url <- "http://www.mass.gov"
 dest.loc <- "data/"
   
-# Read HTML file, extract RTF link name
+# Read HTML file
 doc.html <- htmlParse(source.url)
-doc.links <- xpathSApply(doc.html, "//a/@href")
+
+# Scrape links and town names
+doc.links <- xpathSApply(doc.html, "//a[@class='titlelink']/@href")
 rtf.url <- as.character(doc.links[grep(".*hsicity.*rtf", doc.links)])
+doc.towns <- xpathSApply(doc.html, "//a[@href[contains(., 'hsicity')] and @class='titlelink']", xmlValue)
+doc.towns <- gsub(" \\(RTF\\)", "", doc.towns)
+
+# Assemble URLs to download documents
 rtf.base <- basename(rtf.url)
 get.list <- paste(base.url, rtf.url, sep = "")
 
@@ -20,3 +26,6 @@ for (i in 1:length(get.list)) {
     download.file(get.list[i], dest.file)
     }
 }
+
+# Create data frame of filenames and town names
+towns <- data.frame(town = doc.towns, file = rtf.base)
