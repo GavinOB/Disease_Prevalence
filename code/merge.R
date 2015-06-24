87e081374ed0181992a98d9456ce727ae6ac5f8a
Gavin O'Brien <gobrien@servings.org>
2015-06-24 11:22:54 -0400

    Calculate % difference from state-level prevalence rates

--- code/merge.R
+++ code/merge.R
@@ -2,13 +2,13 @@
 town.rates <- merge(towns.cancer[, c("town", "breast.cancer.rate", "breast.cancer.number")], towns.hiv[, c("town", "HIV.rate", "HIV.number")], by = "town", all = TRUE)
 town.rates <- merge(town.rates, poverty[, c("town", "poverty.rate")], by = "town", all.x = TRUE)
 
-state.rates <- read.table(header = TRUE, text = "
-                   Measure Rate
-                   'Breast Cancer' 134.5
-                   'HIV' 261.0
-                   'Poverty' 11.4
-                   ")
+state <- c(134.5, 261.0, 11.4)
+
+# Calculate % difference from state-level prevalence rates
+town.deviations <- data.frame(town.rates[, 1], mapply("/", town.rates[, c(2, 4, 6)], state))
+names(town.deviations)[1] <- "town"
+
 
 if (!file.exists("data/prevalence.RData")) {
-  save(town.rates, state.rates, file = "data/prevalence.RData")
+  save(town.rates, town.deviations, file = "data/prevalence.RData")
 }
